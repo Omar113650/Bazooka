0acedca65e4e9ce6f8785fefdc12b8e9f74d0999
Omar113650 <omarelhelaly520@gmail.com>
2025-08-02 15:23:12 +0300

    .

--- index.js
+++ index.js
@@ -62,14 +62,9 @@ app.use("/api/v1/cart", cartRoutes);
 app.use(notfound);
 app.use(errorHandler);
 
-// const PORT = 8080||process.env.PORT ;
-// app.listen(PORT, () => {
-//   console.log(` Server running on port ${PORT}`);
-// });
-
-
-
-
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
 
 export default app;
